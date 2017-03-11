f4d25d37d953990beabe306ba60c6794c65c3534
Siying Feng <fengsy@umich.edu>
2017-03-11 16:30:50 -0500

    initialization

---
+++ rpn.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import operator
+
+OPERATORS = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
+
+
+def calculate(arg):
+	stack = list()
+	for operand in arg.split():
+		try:
+			operand = float(operand)
+			stack.append(operand)
+		except:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			operator_fn = OPERATORS[operand]
+			result = operator_fn(arg1, arg2)
+			
+			stack.append(result)
+	return stack.pop()
+
+def main():
+	while True:
+		result = calculate(input('rpn calc> '))
+		print("Result:", result)
+
+if __name__ == '__main__':
+	main()
